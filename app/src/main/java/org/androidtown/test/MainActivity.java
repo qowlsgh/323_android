9d73660cd76711cce1672ea7471961324050c9a9
ghwlsqo819 <ghwlsqo819@gmail.com>
2017-03-23 19:08:11 +0900

    practice Android studio

---
+++ app/src/main/java/org/androidtown/test/MainActivity.java
@@ -0,0 +1,23 @@
+package org.androidtown.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+        public Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.finish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
